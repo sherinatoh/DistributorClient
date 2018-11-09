fc3667d08164e0c575a6724258a772471d3ffef8
turtle96 <angjazlyn@hotmail.com>
2018-11-09 17:06:22 +0800

    move script to controller function check on display, put in placeholder image

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,39 +9,38 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
 
-}]);
-
-// Loading the Key File
-function loadFile(filePath) {
-    var result = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", filePath, false);
-    xmlhttp.send();
-    if (xmlhttp.status === 200) {
-        result = xmlhttp.responseText;
-        console.log("load key success")
+    // Loading the Key File
+    function loadFile(filePath) {
+        var result = null;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", filePath, false);
+        xmlhttp.send();
+        if (xmlhttp.status === 200) {
+            result = xmlhttp.responseText;
+            console.log("load key success")
+        }
+        console.log("File: " + result);
+        return result;
     }
-    console.log("File: " + result);
-    return result;
-}
 
-loadFile("./view1/file.txt");
-var picture = {};
-picture.src = "";
-console.log(picture);
+    loadFile("./view1/file.txt");
+    $scope.picture = "./view1/cup.jpg";
+    console.log($scope.picture);
 
-// Decrypting the image
-var data = new XMLHttpRequest();
-// Put encrypted image URL inside
-data.open('GET', 'http://server.com/pup.jpg.enc.b64', true);
-data.onreadystatechange = function(){
-    if (this.readyState === 4 && this.status === 200) {
-        var dec = CryptoJS.AES.decrypt(data.responseText, "password");
-        var plain = CryptoJS.enc.Base64.stringify(dec);
-        picture.src = "data:image/jpeg;base64," + plain;
-        console.log(picture);
-    }
-};
-data.send();
+    // Decrypting the image
+    var data = new XMLHttpRequest();
+    // Put encrypted image URL inside
+    data.open('GET', 'http://server.com/pup.jpg.enc.b64', true);
+    data.onreadystatechange = function($scope){
+        if (this.readyState === 4 && this.status === 200) {
+            var dec = CryptoJS.AES.decrypt(data.responseText, "password");
+            var plain = CryptoJS.enc.Base64.stringify(dec);
+            $scope.picture = "data:image/jpeg;base64," + plain;
+            console.log(picture);
+        }
+    };
+    data.send();
+});
+
