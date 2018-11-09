2b027a66cf447223ad646300f02340910889220c
turtle96 <angjazlyn@hotmail.com>
2018-11-09 16:42:06 +0800

    move javascript over xmlhttprequest not sending?

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,3 +12,36 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', [function() {
 
 }]);
+
+// Loading the Key File
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status === 200) {
+        result = xmlhttp.responseText;
+        console.log("load key success")
+    }
+    console.log("File: " + result);
+    return result;
+}
+
+loadFile("./view1/file.txt");
+var picture = {};
+picture.src = "";
+console.log(picture);
+
+// Decrypting the image
+var data = new XMLHttpRequest();
+// Put encrypted image URL inside
+data.open('GET', 'http://server.com/pup.jpg.enc.b64', true);
+data.onreadystatechange = function(){
+    if (this.readyState === 4 && this.status === 200) {
+        var dec = CryptoJS.AES.decrypt(data.responseText, "password");
+        var plain = CryptoJS.enc.Base64.stringify(dec);
+        picture.src = "data:image/jpeg;base64," + plain;
+        console.log(picture);
+    }
+};
+data.send();
